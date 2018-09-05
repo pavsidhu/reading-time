98076d3cae10d14a3bfeb5d2fc1ff871e63022d7
Pav Sidhu <pavsidhu@outlook.com>
2018-09-05 22:17:08 +0100

    Add options to type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,20 @@
-declare module "reading-time" {
-  function readingTime(text: string): {
-    text: string;
-    time: number;
-    words: number;
-    minutes: number;
-  };
-  
-  namespace readingTime {}
+declare module 'reading-time' {
+  function readingTime(
+    text: string,
+    options?: {
+      wordsPerMinute?: number
+      wordBound?: (c: string) => boolean
+    }
+  ): {
+    text: string
+    time: number
+    words: number
+    minutes: number
+  }
 
-  export = readingTime;
+  namespace readingTime {
+
+  }
+
+  export = readingTime
 }
